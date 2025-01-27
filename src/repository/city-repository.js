fb9091fb96139530b5093f59f55582bb953f97e3
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-01-27 18:49:58 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,7 +6,8 @@ class CityRepository{
             const city = await City.create({ name });
             return city;
         } catch(error){
-            throw(error);
+            console.log("Something went wrong in the repository layer");
+            throw{error};
         }
     }
 
@@ -17,8 +18,35 @@ class CityRepository{
                     id:cityId
                 }
             });
+            return true;
         } catch (error) {
-            throw {error};
+            console.log("Something went wrong in the repository layer");
+            throw{error};
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data , {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        }
+        catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw{error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId)
+        } 
+        catch (error){
+            console.log("Something went wrong in the repository layer");
+            throw{error};
         }
     }
 
