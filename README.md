c3182fbd7ff96ad876834d90d86d9b0bc6532939
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-01-28 21:04:56 +0530

    Added city Controller

--- README.md
+++ README.md
@@ -21,4 +21,15 @@
 
 ```
 
-- Once added db config as listed above , go to your src folder from terminal/bash and execcute `npx sequelize db:create`
+- Once added db config as listed above , go to your src folder from terminal/bash and execute `npx sequelize db:create` and then execute `npx sequelize db:migrate`
+
+## DB Design
+
+    - Airplane Table
+    - Flight
+    - Airport
+    - City
+
+    - A flight belons to an airplane but one airplane can be used in multiple flights
+    - A city has many airports but one airport belongs to a city
+    - One airport can have many flights, but a flight belongs to one airport
--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,19 +1,85 @@
-const {CityService} = require('../services/index');
+const {CityService } = require('../services/index');
 
-const create = (req,res) =>{
+const cityService = new CityService();
 
+const create = async (req,res) =>{
+    try {
+        const city = await cityService.createCity(req.body);
+        return res.status(201).json({
+            data: city,
+            success: true,
+            message: "Sucessfully created a city",
+            err: {}
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to create a city',
+            err: error
+        });
+    }
 }
 
-const destroy  = (req,res) =>{
-    
+const destroy  = async (req,res) =>{
+    try {
+        const city = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Sucessfully deleted a city",
+            err: {}
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to delete the city',
+            err: error
+        });
+    }
 }
 
-const get = (req,res) =>{
-    
+const get = async (req,res) =>{
+    try {
+        const city = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Sucessfully fetched a city",
+            err: {}
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to get the city',
+            err: error
+        });
+    }
 }
 
-const update = (req,res) =>{
-    
+const update = async (req,res) =>{
+    try {
+        const city = await cityService.deleteCity(req.params.id, req.body);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Sucessfully fetched a city",
+            err: {}
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to update the city',
+            err: error
+        });
+    }
 }
 
 module.exports = {
