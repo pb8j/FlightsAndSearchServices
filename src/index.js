cf87f5ee3f2cf0354edd1eccfa8ce54d64ba6f4a
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-01-28 22:14:19 +0530

    Added routers to complete create api

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverconfig');
 
+const ApiRoutes = require('./routes/index');
+
 const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async () => {
@@ -13,11 +15,11 @@ const setupAndStartServer = async () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api' , ApiRoutes);
+
     app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
-        
-        const repo = new CityRepository;
-        repo.createCity({name:"Nagpur"});
     });
 }
 
