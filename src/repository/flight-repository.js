03e13fe5a8a942460308427be686447829964be8
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-02-16 22:25:50 +0530

    Completed flight update api

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -68,6 +68,20 @@ class FlightRepository {
         }
     }
 
+    async updateFlights(FlightId,data) {
+        try {
+            await Flights.update(data, {
+                where: {
+                    id:FlightId
+                }
+            });
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
 
 }
 
